9a51409c9bd37d9f313b4ba57d171670fd76592f
wladimov <info@wladimov.com>
2024-11-09 12:06:00 -0500

    feat: change users service path

--- app/routes/user.router.js
+++ app/routes/user.router.js
@@ -8,7 +8,7 @@ import {
 const router = express.Router();
 
 router.post("/", saveUser);
-router.get("/all", getUsers);
+router.get("/all-users", getUsers);
 router.delete("/:id", deleteUser);
 
 export default router;
